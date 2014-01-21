6971cdd97afd23755c101bf5456f1585737f2b44
luis <luis@luis-ATIV6>
2014-01-21 19:41:31 -0200

    ajuste no sql.

--- sql/1.0.0.sql
+++ sql/1.0.0.sql
@@ -1,47 +1,36 @@
 ﻿
 CREATE TABLE activity (
     id serial primary key,
-    type_id integer,
+    type_id integer not null,
     user_id integer,
     "name" character varying(255) NOT NULL,
-    priority integer NOT NULL,
+    priority integer NOT NULL default 0,
     description character varying(255) NOT NULL,
-    creation_date timestamp(0) without time zone NOT NULL default now(),
-    last_update timestamp(0) without time zone NOT NULL default now(),
-    active boolean NOT NULL,
-    status integer NOT NULL
+    creation_date timestamp(0) without time zone DEFAULT now() NOT NULL,
+    last_update timestamp(0) without time zone DEFAULT now() NOT NULL,
+    active boolean NOT NULL default true,
+    status integer NOT NULL default 0
 );
 
 
---
--- TOC entry 162 (class 1259 OID 16724)
--- Dependencies: 5
--- Name: activity_interaction; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
 
 CREATE TABLE activity_interaction (
     id serial primary key,
     user_id integer,
     activity_id integer,
     description character varying(255) NOT NULL,
-    creation_date timestamp(0) without time zone NOT NULL,
+    creation_date timestamp(0) without time zone DEFAULT now() NOT NULL,
     allocated_time time(0) without time zone NOT NULL
 );
 
 
 
---
--- TOC entry 161 (class 1259 OID 16716)
--- Dependencies: 5
--- Name: activity_type; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
 CREATE TABLE activity_type (
     id serial primary key,
     name character varying(255) NOT NULL,
     description character varying(255) NOT NULL,
-    creation_date timestamp(0) without time zone NOT NULL,
-    last_update timestamp(0) without time zone NOT NULL,
+    creation_date timestamp(0) without time zone DEFAULT now() NOT NULL,
+    last_update timestamp(0) without time zone DEFAULT now() NOT NULL,
     active boolean NOT NULL
 );
 
@@ -49,62 +38,27 @@ CREATE TABLE activity_type (
 
 CREATE TABLE user_work_day (
     id serial primary key,
-    user_id integer,
+    user_id integer not null,
     date date NOT NULL,
-    day_active_hour time(0) without time zone NOT NULL
+    day_active_hour time(0) without time zone NOT NULL,
+    constraint uk_user_date unique(user_id,date) 
 );
 
-
---
--- TOC entry 163 (class 1259 OID 16731)
--- Dependencies: 5
--- Name: users; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
 
 CREATE TABLE users (
     id serial primary key,
     name character varying(255) NOT NULL,
-    cpf character varying(255) NOT NULL,
+    cpf character varying(255),
     email character varying(255) NOT NULL,
     password character varying(255) NOT NULL,
     day_active_hour time(0) without time zone NOT NULL,
     active boolean NOT NULL,
-    creation_date timestamp(0) without time zone NOT NULL,
-    last_access timestamp(0) without time zone NOT NULL,
-    last_update timestamp(0) without time zone NOT NULL
+    creation_date timestamp(0) without time zone DEFAULT now() NOT NULL,
+    last_access timestamp(0) without time zone,
+    last_update timestamp(0) without time zone DEFAULT now() NOT NULL
 );
 
-CREATE INDEX idx_390397ab81c06096 ON activity_interaction USING btree (activity_id);
 
-
-CREATE INDEX idx_390397aba76ed395 ON activity_interaction USING btree (user_id);
-
-
-CREATE INDEX idx_7f439a0ca76ed395 ON user_work_day USING btree (user_id);
-
-
-
-CREATE INDEX idx_ac74095aa76ed395 ON activity USING btree (user_id);
-
-
-CREATE INDEX idx_ac74095ac54c8c93 ON activity USING btree (type_id);
-
-
-ALTER TABLE ONLY activity_interaction
-    ADD CONSTRAINT fk_390397ab81c06096 FOREIGN KEY (activity_id) REFERENCES activity(id);
-
-
-ALTER TABLE ONLY activity_interaction
-    ADD CONSTRAINT fk_390397aba76ed395 FOREIGN KEY (user_id) REFERENCES users(id);
-
-
-ALTER TABLE ONLY user_work_day
-    ADD CONSTRAINT fk_7f439a0ca76ed395 FOREIGN KEY (user_id) REFERENCES users(id);
-
-
-ALTER TABLE ONLY activity
-    ADD CONSTRAINT fk_ac74095aa76ed395 FOREIGN KEY (user_id) REFERENCES users(id);
-
-
-ALTER TABLE ONLY activity
-    ADD CONSTRAINT fk_ac74095ac54c8c93 FOREIGN KEY (type_id) REFERENCES activity_type(id);
+INSERT INTO users (name, cpf, email, password, day_active_hour, active, creation_date, last_access, last_update)
+ VALUES ('Administrador', NULL, 'admin@admin.com', '$2a$08$8kxMyHlhIAYRnhqy5a0q5OMbVf8hkK05Bt9qK0HEeY.Nk9RQ6B3AG',
+ '07:30:00', true, '2014-01-20 10:06:48', '2014-01-21 19:32:07', '2014-01-21 19:32:07');
