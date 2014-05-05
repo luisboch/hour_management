d0691868c7f9381bc5556da25f50685da90138dc
luis <luis@localhost>
2014-05-05 16:42:28 -0300

    added controls to user close and finish action.

---
+++ public/js/activities/script.js
@@ -0,0 +1,46 @@
+
+function finishAction(a, t) {
+    try {
+        $('#finish_end_time').val(t);
+        $('#action_id').val(a);
+        $('#action_submit').submit();
+    } catch (e) {
+        alert(e);
+    }
+    return false;
+}
+
+var opts = {
+    'minTime': '06:00am',
+    'maxTime': '08:00pm',
+    'timeFormat': 'H:i:s'
+};
+
+$('#action_end_time').timepicker(opts);
+$('#action_start_time').timepicker(opts);
+$('.action-input-time').timepicker(opts);
+$('#status').change(function() {
+    var chkActive = $('#active').is(':checked');
+    var newval = $('#status').val();
+    if (newval === '0' && chkActive) {
+        $('#newActionPanel').slideDown('fast');
+    } else {
+        $('input[name=action_start_time]').val('');
+        $('input[name=action_end_time]').val('');
+        $('#newActionPanel').slideUp('fast');
+    }
+});
+
+$('#active').change(function() {
+    var chkActive = $('#active').is(':checked');
+    var newval = $('#status').val();
+    if (newval === '0' && chkActive) {
+        $('#newActionPanel').slideDown('fast');
+    } else {
+        $('input[name=action_start_time]').val('');
+        $('input[name=action_end_time]').val('');
+        $('#newActionPanel').slideUp('fast');
+    }
+
+});
+
